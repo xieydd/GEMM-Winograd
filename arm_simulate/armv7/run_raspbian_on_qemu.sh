59a7b26e5c07a117eb40b65179ba7c502dfa46dd
xieydd <xieydd@gmail.com>
2020-02-27 15:47:04 +0800

    Add osx raspberry armv7 simulator, can download 2018-11-13-raspbian-stretch-lite.zip from http://downloads.raspberrypi.org/raspbian_lite/images/raspbian_lite-2018-11-15/2018-11-13-raspbian-stretch-lite.zip

---
+++ arm_simulate/armv7/run_raspbian_on_qemu.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+qemu-system-arm \
+        -M vexpress-a9 \
+        -m 1024 \
+        -smp 4 \
+        -kernel qemu_files/zImage \
+        -dtb qemu_files/vexpress-v2p-ca9.dtb \
+        -sd qemu_files/2018-11-13-raspbian-stretch-lite.img \
+        -append "console=ttyAMA0,115200 root=/dev/mmcblk0p2" \
+        -serial stdio \
+        -net nic -net user,hostfwd=tcp::2222-:22
